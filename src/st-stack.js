b936121254a6488e77aa9b0178cecbade4a632ff
irina-mokh <imoh91@gmail.com>
2021-10-13 17:23:03 +0300

    feat:  implement st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   constructor() {
+     this.items = [];
+   }
+  push(el) {
+    return this.items.unshift(el);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items.shift();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items[0];
   }
 }
